e9a6073a71021fb653545711c902c658124a224f
Roberto Valenzuela <robertoandres958@gmail.com>
2022-02-11 23:31:15 -0300

    Se agrega la funcion de mediana y moda

--- mediana.js
+++ mediana.js
@@ -1,3 +1,72 @@
+const btnAgregar = document.getElementById('btnAgregar');
+const btnCalcular = document.getElementById('btnCalcular');
+const fragment = document.createDocumentFragment();
+let lista = document.querySelector('.lista');
+let resultado = document.querySelector('.resultado');
+
+let listNumber = [];
+
+btnAgregar.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const inputNumber = parseInt(document.getElementById("inputNumber").value);
+    listNumber.push(inputNumber);
+    console.log(listNumber);
+
+    mostrarDatos();
+
+    document.getElementById("inputNumber").value = "";
+});
+
+
+
+btnCalcular.addEventListener('click', (e) => {
+    let mediana;
+
+    e.preventDefault();    
+
+    //Ordena la lista de menor a mayor
+    ordenarLista(listNumber);
+
+    //Mitadlista me da el lugar del objeto
+    let mitadLista = Math.floor(listNumber.length / 2);
+
+
+    if(esPar(listNumber.length)){
+        //Mediana par
+        //Toma el primer valor de la mediana
+        const elemento1 = listNumber[mitadLista - 1];
+        console.log(elemento1);
+        //Toma el siguente resultado de la mediana
+        const elemento2 = listNumber[mitadLista];
+        console.log(elemento2);
+    
+        const promedioElementos1y2 = calcularMediaAritmetica([elemento1, elemento2]);
+    
+        mediana = promedioElementos1y2;
+    }else{
+        //Mediana inpar
+        //Me devuelve el valor que esta en esa posición
+        mediana = listNumber[mitadLista];
+    }
+
+    const totalTexto = document.createElement('p');
+    totalTexto.textContent = `La mediana de los ${listNumber.length} números anteriores es: ${mediana}`;
+    fragment.appendChild(totalTexto);
+
+    resultado.appendChild(fragment);
+})
+
+function mostrarDatos() {
+    lista.innerHTML = '';
+    listNumber.forEach(element => {
+        const datoLista = document.createElement('li');
+        datoLista.textContent = element;
+        fragment.appendChild(datoLista);
+    })
+    lista.appendChild(fragment);
+}
+
 function ordenarLista(lista){
     lista.sort(comparacion);
 
@@ -20,26 +89,6 @@ const promedioLista = sumaLista / lista.length;
 return promedioLista;
 }
 
-
-const lista1 = [
-    100,
-    500,
-    150000,
-    900,
-    4000000
-];
-
-
-//Ordena la lista de menor a mayor
-ordenarLista(lista1);
-console.log(lista1);
-
-//parseInt fuerza el resultado a un numero entero
-//Mitadlista me da el lugar del objeto
-const mitadLista1 = parseInt(lista1.length / 2);
-
-
-
 function esPar(numero){
     //modulo de dos es cero
     if(numero % 2 === 0){
@@ -47,22 +96,4 @@ function esPar(numero){
     }else{
         return false;
     }
-}
-
-let mediana;
-
-if(esPar(lista1.length)){
-    //Mediana par
-    //Toma el primer valor de la mediana
-    const elemento1 = lista1[mitadLista1 - 1];
-    //Toma el siguente resultado de la mediana
-    const elemento2 = lista1[mitadLista1];
-
-    const promedioElementos1y2 = calcularMediaAritmetica([elemento1, elemento2]);
-
-    mediana = promedioElementos1y2;
-}else{
-    //Mediana inpar
-    //Me devuelve el valor que esta en esa posición
-    mediana = lista1[mitadLista1];
 }
--- moda.js
+++ moda.js
@@ -1,38 +1,69 @@
-const lista1 = [
-1,
-2,
-3,
-2,
-2,
-3,
-1,
-5,
-2
-];
+const btnAgregar = document.getElementById('btnAgregar');
+const btnCalcular = document.getElementById('btnCalcular');
+const fragment = document.createDocumentFragment();
+let lista = document.querySelector('.lista');
+let resultado = document.querySelector('.resultado');
 
-const lista1Count = {};
+let listNumber = [];
+const listNumberCount = [];
 
-//map nos ayuda a recorrer
-lista1.map(
-    //anonima
-    function(elemento){
-        //Si encuentra un elemento lo va a sumar con el siguente que encuentre lo almacena en otra lista
-        if(lista1Count[elemento]){
-        lista1Count[elemento] += 1;
-        }else{
-        lista1Count[elemento] = 1;
+btnAgregar.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const inputNumber = parseInt(document.getElementById("inputNumber").value);
+    listNumber.push(inputNumber);
+    console.log(listNumber);
+
+    mostrarDatos();
+
+    document.getElementById("inputNumber").value = "";
+})
+
+btnCalcular.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    listNumber.map(
+        //anonima
+        function(elemento){
+            //Si encuentra un elemento lo va a sumar con el siguente que encuentre lo almacena en otra lista
+            if(listNumberCount[elemento]){
+            listNumberCount[elemento] += 1;
+            }else{
+            listNumberCount[elemento] = 1;
+            }
         }
-    }
-);
+    );
+    console.log(listNumber);
 
-//Almacena en un array nuestro objeto
-// y... .sort ordena los elementos por la segunda posicion(cantidad de veces que se repite)
-const lista1Array = Object.entries(lista1Count).sort(
-    function (elementoA, elementoB){
-        //Compara la segunda posicion
-        return elementoA[1] - elementoB[1];
-    }
-);
+    //Almacena en un array nuestro objeto
+    // y... .sort ordena los elementos por la segunda posicion(cantidad de veces que se repite)
+    const lista1Array = Object.entries(listNumberCount).sort(
+        function (elementoA, elementoB){
+            //Compara la segunda posicion
+            return elementoA[1] - elementoB[1];
+        }
+    );
 
-//Me almacaneca el valor del ultimo array
-const moda = lista1Array[lista1Array.length - 1];
+    //Almacena el valor del ultimo array
+    const moda = lista1Array[lista1Array.length - 1];
+
+    console.log(lista1Array);
+
+    const totalTexto = document.createElement('p');
+    totalTexto.textContent = `Cantidades: ${lista1Array}
+                                La moda es: ${moda}`;
+    fragment.appendChild(totalTexto);
+
+    resultado.appendChild(fragment);
+});
+
+
+function mostrarDatos() {
+    lista.innerHTML = '';
+    listNumber.forEach(element => {
+        const datoLista = document.createElement('li');
+        datoLista.textContent = element;
+        fragment.appendChild(datoLista);
+    });
+    lista.appendChild(fragment);
+};
